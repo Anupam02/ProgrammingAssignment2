ca16db4e282298d54420dcbf2858eaf2e8ce2222
Anupam Patel <patel.anupam02@gmail.com>
2015-03-22 12:13:58 +0530

    support for Inverse of non square matrices added

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Program to use cached value of Inverse operation in Matrix
+#
+## makeCacheMatrix -> used to create a Matrix and cache its inverse
+#
+## cacheSolve -> used to get the cached Value of Matrix ,
+##               created by makeCacheMatrix
 
-## Write a short comment describing this function
+## makeCacheMatrix function is taking an matrix and saves it's cached value
+## every time it gets called
 
 makeCacheMatrix <- function(x = matrix()) {
+    library(MASS)
     inv = NULL
     
     set <- function(y) {
         x <<- y
         inv <<- NULL
     }
+
     get <- function() x
-    setinv <- function(solve) inv <<- solve
+
+    setinv <- function(ginv) inv <<- ginv
+
     getinv <- function() inv
+
     list(set = set, get = get,
         setinv = setinv,
         getinv = getinv)
@@ -20,18 +30,27 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is used to get the cached value of Inverse of a Matrix,
+## created by makeCacheMatrix,
+## which is a complex operation which will save the running time
+## for going through the Inverse calculation every single time 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  ## Return a matrix that is the inverse of 'x'
   inv <- x$getinv()
+
+  ## if there is cached Matrix it will return the cached value of its Inverse
   if (!is.null(inv)) {
     message("getting cached data")
     return (inv)
     
   }
+
+  ##if inverse is not calculated in makeCacheMatrix (setinv) ,
+  ## it will calculate the inverse of it
   data <- x$get()
-  inv <- solve(data, ...)
-  x$setmean(inv)
+  inv <- ginv(data, ...)
+  x$setinv(inv)
   inv
 }
